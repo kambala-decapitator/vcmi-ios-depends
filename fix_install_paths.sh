3591bb69365423f21f5f0e072db11c3eeb5588ba
Andrey Filipenkov <decapitator@ukr.net>
2021-11-28 19:52:41 +0300

    add script to fix absolute paths in Boost's cmake files

---
+++ fix_install_paths.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+for dir in iphoneos iphonesimulator; do
+	cmakeBaseDir="$dir/lib/cmake"
+	boostBaseDir="$cmakeBaseDir/boost_"
+	currentInstallPath="$(fgrep 'if(EXISTS "' "$boostBaseDir"date_time-*/boost_date_time-config.cmake | cut -d \" -f 2)"
+	newInstallPath="$(pwd)/$cmakeBaseDir"
+	echo "fixing Boost install path: $currentInstallPath => $newInstallPath"
+	for boostDir in "$boostBaseDir"*; do
+		sed -i '' "s|$currentInstallPath|$newInstallPath|g" $boostDir/*.cmake
+	done
+done
